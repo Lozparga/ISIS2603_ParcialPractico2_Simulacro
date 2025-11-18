3b89ba55f0745f9878371ac4f32ca9c0711c01f8
Ariana Lozano <al.lozanop1@uniandes.edu.co>
2025-11-18 09:41:04 -0500

    Ingrediente mas usado

--- src/app/recipe/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe/recipe-detail/recipe-detail.component.ts
@@ -19,18 +19,38 @@ export class RecipeDetailComponent implements OnInit {
     private recipeService: RecipeService
   ) {}
 
+  ingredienteMasUsado: string = "";
+
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
 
     this.recipeService.getRecipeDetail(id).subscribe({
       next: (data) => {
         this.recipe = data;
+        this.calcularIngredienteMasUsado();
       },
       error: (err) => {
         console.error('Error cargando detalle:', err);
       }
     });
   }
+
+  calcularIngredienteMasUsado(){
+    if (!this.recipe) return; 
+
+    let mayor =0;
+    let nombreMayor ="";
+
+    this.recipe.ingredientes.forEach(ing => {
+      const numero = parseInt(ing.cantidad.split(' ')[0]);
+      if (numero > mayor){
+        mayor = numero;
+        nombreMayor = ing.nombre;
+      }  
+    });
+    this.ingredienteMasUsado= nombreMayor;
+  }
+
   goBack() {
   this.router.navigate(['/recipe']);
   }
